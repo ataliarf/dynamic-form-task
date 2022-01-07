4e51a9d5fa98dc4de4d4c56747593c77c3da1fb0
rotemzif1 <86773508+rotemzif1@users.noreply.github.com>
2022-01-07 12:47:36 +0200

    Update formScheme.ts

--- src/configuration/formScheme.ts
+++ src/configuration/formScheme.ts
@@ -17,7 +17,7 @@ export const formScheme : {formFields: IField[]} = {
             label: 'First Name',
             placeholder: 'First Name',
             type: 'string',
-            width: '0.5',
+            width: '50%',
             required: true
         },
         {
@@ -25,7 +25,7 @@ export const formScheme : {formFields: IField[]} = {
             label: 'Last Name',
             placeholder: 'Last Name',
             type: 'string',
-            width: '0.5',
+            width: '50%',
             required: true
 
         },
@@ -34,7 +34,7 @@ export const formScheme : {formFields: IField[]} = {
             label: 'Your Phone Number',
             placeholder: '+972 ...',
             type: 'phone',
-            width: '0.5',
+            width: '120',
             required: true
         },
         {
@@ -42,20 +42,20 @@ export const formScheme : {formFields: IField[]} = {
             label: 'Your Email Address',
             placeholder: 'myEmail@...',
             type: 'email',
-            width: '0.5',
+            width: '120',
             required: true
         },
         {
             key: 'gender',
             label: 'Your Gender',
             type: 'gender',
-            width: '1'
+            width: '100%'
         },
         {
             key: 'consentApproval',
             label: 'Do you agree to the terms of use?',
             type: 'boolean',
-            width: '1',
+            width: '100%',
             required: true
         },
         {
@@ -63,7 +63,7 @@ export const formScheme : {formFields: IField[]} = {
             label: 'Please select your company size',
             type: 'options',
             options: ['1-3', '4-20', '20-100', '100-500'],
-            width: '1'
+            width: '50%'
         },
     ]
 }
